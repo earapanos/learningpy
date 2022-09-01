7d8403b2b8df258cddd996179f78df5a0f3338c2
Eduardo Adriani Rapanos <52800638+earapanos@users.noreply.github.com>
2022-09-01 11:12:24 -0300

    Update imc_final.py

--- imc_final.py
+++ imc_final.py
@@ -6,22 +6,22 @@ altura = float(input('Digite a sua altura em metros: '))
 peso = float(input('Digite o seu peso em kilogramas: '))
 imc = float(peso/(altura*altura))
 if ((imc)) < 18.5:
-    print('{:.2f}'.format(imc))
+    print('Seu IMC é: ''{:.2f}'.format(imc))
     print('Você está abaixo do peso segundo a OMS.')
 elif (((imc)) >= 18.5 and ((imc)) < 24.9):
-    print('{:.2f}'.format(imc))
+    print('Seu IMC é: ''{:.2f}'.format(imc))
     print('Você está com o peso normal segundo a OMS.')
 elif (((imc)) >= 24.9 and ((imc)) < 29.9):
-    print('{:.2f}'.format(imc))
+    print('Seu IMC é: ''{:.2f}'.format(imc))
     print('Você está com excesso de peso segundo a OMS.')
 elif (((imc)) >= 29.9 and ((imc)) < 34.9):
-    print('{:.2f}'.format(imc))
+    print('Seu IMC é: ''{:.2f}'.format(imc))
     print('Você está com obesidade classe I segundo a OMS.')
 elif (((imc)) >= 34.9 and ((imc)) < 39.9):
-    print('{:.2f}'.format(imc))
+    print('Seu IMC é: ''{:.2f}'.format(imc))
     print('Você está com obesidade classe II segundo a OMS.')
 elif (((imc)) >= 39.9):
-    print('{:.2f}'.format(imc))
+    print('Seu IMC é: ''{:.2f}'.format(imc))
     print('Você está com obesidade classe III segundo a OMS.')
 print('\n')
 input('Digite <enter> para encerrar.')
